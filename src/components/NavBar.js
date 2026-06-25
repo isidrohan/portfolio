2563aecf917970b975d7a8d2c358af37ea4d1952
Siddharth Rohan <sidrohanme2@gmail.com>
2026-06-25 17:02:41 +0530

    Fix navbar accessibility warnings

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,60 +1,118 @@
-// import { NavLink } from "react-router-dom"
-import './NavBar.css'
-import  React,{useState} from 'react';
-import menu from '../image/menu.png'
-import cross from '../image/cross.png'
-import { useRef } from 'react';
-import {Home} from '../pages/Home'
-import {About} from '../pages/About'
-import {Projects} from '../pages/Projects'
-import {Contact} from '../pages/Contact'
-import { Expertise } from '../pages/Expertise';
-import { Footer } from '../pages/Footer';
+import "./NavBar.css";
+import React, { useState, useRef } from "react";
+import menu from "../image/menu.png";
+import cross from "../image/cross.png";
+import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+import { Projects } from "../pages/Projects";
+import { Contact } from "../pages/Contact";
+import { Expertise } from "../pages/Expertise";
+import { Footer } from "../pages/Footer";
 
-export const NavBar = (props) =>{
-    const [openmenu, setOpenmenu] = useState(false);
-    const home = useRef();
-    const about = useRef();
-    const expertise = useRef();
-    const project = useRef();
-    const contact = useRef();
+export const NavBar = () => {
+  const [openmenu, setOpenmenu] = useState(false);
 
-    const scrollHandler = (eleRef) => {
-        console.log(eleRef);
-        window.scrollTo({top: eleRef.current.offsetTop, behavior: "smooth"});
-    }
+  const home = useRef();
+  const about = useRef();
+  const expertise = useRef();
+  const project = useRef();
+  const contact = useRef();
 
-    return(
-        <>
-        
-        <header>
-            <div className="container">
-                <div className="logo">
-                    {/* <NavLink to='/' className='logo-item'>WELCOME!</NavLink> */}
-                    <li> <a className="logo-item" >WELCOME!</a></li>
-                </div>
-                <nav className='menu'>
-                    <img className='menu-btn' src={openmenu ? cross : menu } alt="" onClick={()=>setOpenmenu(!openmenu)} />
-                    <ul className={`menu-items ${openmenu ? 'menu-open' : ''}`}>
-                        
-                        <li><a className="list-item" onClick={()=>{scrollHandler(home)}}>HOME</a></li>
-                        <li><a className="list-item" onClick={()=>{scrollHandler(about)}}>ABOUT</a></li>
-                        <li><a className="list-item" onClick={()=>{scrollHandler(expertise)}}>EXPERTISE</a></li>
-                        <li><a className="list-item" onClick={()=>{scrollHandler(project)}}>PROJECTS</a></li>
-                        <li><a className="list-item" onClick={()=>{scrollHandler(contact)}}>CONTACT</a></li>
-                    </ul>
-                </nav>
-            </div>
-            
-        </header>
-        <Home nameforRef={home}/>
-        <About nameforRef={about}/>
-        <Expertise nameforRef={expertise} />
-        <Projects nameforRef={project}/>
-        <Contact nameforRef={contact}/>
-        <Footer/>
-    
-      </>
-    )
-}
+  const scrollHandler = (eleRef) => {
+    window.scrollTo({
+      top: eleRef.current.offsetTop,
+      behavior: "smooth",
+    });
 
+    // Close menu on mobile after clicking a link
+    setOpenmenu(false);
+  };
+
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="logo">
+            <li>
+              <button
+                type="button"
+                className="logo-item"
+                onClick={() => scrollHandler(home)}
+              >
+                WELCOME!
+              </button>
+            </li>
+          </div>
+
+          <nav className="menu">
+            <img
+              className="menu-btn"
+              src={openmenu ? cross : menu}
+              alt="menu"
+              onClick={() => setOpenmenu(!openmenu)}
+            />
+
+            <ul className={`menu-items ${openmenu ? "menu-open" : ""}`}>
+              <li>
+                <button
+                  type="button"
+                  className="list-item"
+                  onClick={() => scrollHandler(home)}
+                >
+                  HOME
+                </button>
+              </li>
+
+              <li>
+                <button
+                  type="button"
+                  className="list-item"
+                  onClick={() => scrollHandler(about)}
+                >
+                  ABOUT
+                </button>
+              </li>
+
+              <li>
+                <button
+                  type="button"
+                  className="list-item"
+                  onClick={() => scrollHandler(expertise)}
+                >
+                  EXPERTISE
+                </button>
+              </li>
+
+              <li>
+                <button
+                  type="button"
+                  className="list-item"
+                  onClick={() => scrollHandler(project)}
+                >
+                  PROJECTS
+                </button>
+              </li>
+
+              <li>
+                <button
+                  type="button"
+                  className="list-item"
+                  onClick={() => scrollHandler(contact)}
+                >
+                  CONTACT
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <Home nameforRef={home} />
+      <About nameforRef={about} />
+      <Expertise nameforRef={expertise} />
+      <Projects nameforRef={project} />
+      <Contact nameforRef={contact} />
+      <Footer />
+    </>
+  );
+};
